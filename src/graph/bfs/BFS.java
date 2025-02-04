d5b5ba7e39af00ea277c9d0d2471f4c38aeb69d3
유종화 <sola201@naver.com>
2025-02-04 17:37:42 +0900

    그래프는 아직 어려운 이론 같음...

---
+++ src/graph/bfs/BFS.java
@@ -0,0 +1,46 @@
+package graph.bfs;
+
+import java.util.*;
+
+public class BFS {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        int n = 7;
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[1].add(2);
+        graph[1].add(3);
+        graph[2].add(4);
+        graph[2].add(5);
+        graph[3].add(6);
+        graph[3].add(7);
+
+        System.out.println("BFS 탐색 결과:");
+        bfs(1);
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : graph[node]) {
+                if (!visited[neighbor]) {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+    }
+}
